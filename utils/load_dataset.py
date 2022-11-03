d87302b1bac9dbd8fa1adfcb7c9ae01c7cab01db
nigel <nigel@cs.umass.edu>
2022-11-03 17:59:52 -0400

    load dataset for lm training

---
+++ utils/load_dataset.py
@@ -0,0 +1,28 @@
+"""
+Load dataset splits for model training.
+"""
+
+import os
+import json
+
+
+RAW_DIR = "./data/"
+
+
+def load_dataset(data_folder="train_val_split_json", debug=False):
+    dir_name = os.path.join(RAW_DIR, data_folder) 
+
+    data = {}
+    names = ["train", "val"]
+    for name in names:
+        filename = os.path.join(dir_name, "{}.json".format(name))
+        with open(filename, "r") as f:
+            data[name] = json.load(f)
+
+    # Debug with less data
+    if(debug):
+        data["train"] = data["train"][:8]
+        data["val"] = data["val"][:8]
+        data["test"] = data["test"][:8]
+
+    return data
