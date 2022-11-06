8109de7a143437a37ead1be269d0c739866c9b40
Nish-19 <nischalrokz19@gmail.com>
2022-11-06 00:55:49 +0000

    update args finetune

--- code/finetune/t5_finetune.py
+++ code/finetune/t5_finetune.py
@@ -1,7 +1,15 @@
 # %%
+'''
+python -m code.finetune.t5_finetune \
+    -M t5-small \
+    -N t5_small
+'''
+
+# %%
 import re
 import wandb, os
 from collections import defaultdict
+import argparse
 import statistics
 import pandas as pd
 import torch
@@ -11,18 +19,15 @@ from transformers import AdamW, get_linear_schedule_with_warmup
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor
+from pytorch_lightning.callbacks import EarlyStopping, LearningRateMonitor, ModelCheckpoint
 
 os.environ['WANDB_NOTEBOOK_NAME'] = 'FinetuneT5'
-
-
 
 # %%
 def clean_str(text):
     # Replace double quotes with single quotes
     # Remove non breaking spaces (\u00A0), etc
     text = re.sub(r"\s+", " ", text)
-
     return text.strip()
 
 
@@ -107,9 +112,9 @@ def get_dataloader(batch_size, dataset, datatype='train'):
 
 # %%
 class FinetuneT5(pl.LightningModule):
-    def __init__(self, lr=5e-5, num_train_epochs=5, warmup_steps=1000):
+    def __init__(self, model_name, lr=3e-4, num_train_epochs=5, warmup_steps=1000):
         super().__init__()
-        self.model = T5ForConditionalGeneration.from_pretrained("t5-small")
+        self.model = T5ForConditionalGeneration.from_pretrained(model_name)
         self.hparams.max_epochs = num_train_epochs
         self.hparams.num_train_epochs = num_train_epochs
         self.hparams.warmup_steps = warmup_steps
@@ -172,77 +177,82 @@ class FinetuneT5(pl.LightningModule):
         return valid_dataloader
 
 # %%
-story_file = '../../data/original/source_texts.csv'
-story_df = pd.read_csv(story_file)
-# Train-Val split
-train_file = '../../data/train_val_split_csv/train.csv'
-train_df = pd.read_csv(train_file)
-val_file = '../../data/train_val_split_csv/val.csv'
-val_df = pd.read_csv(val_file)
 
-train_story, train_answer, train_question = get_parallel_corpus(train_df, story_df)
-val_story, val_answer, val_question = get_parallel_corpus(val_df, story_df)
+def add_params():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-B", "--batch_size", type=int, default=8, help="Batch size for training the T5 Model")
+    parser.add_argument("-L", "--learning_rate", type=float, default=3e-4, help="Learning Rate for training the T5 Model")
+    parser.add_argument("-E", "--num_epochs", type=int, default=5, help="Total Number of Epochs")
+    parser.add_argument("-M", "--model_name", type=str, default="t5-small", help="Variant of the T5 model for finetuning")
+    parser.add_argument("-N", "--run_name", type=str, default="t5-small", help="Name of the Run (Used in storing the model)")
+    params = parser.parse_args()
+    
+    return params
+
 
 # %%
-# print stats
-print('Train Set')
-get_stats(train_story, train_answer, train_question)
+if __name__ == '__main__':
+    args = add_params()
 
-print('Valid Set')
-get_stats(val_story, val_answer, val_question)
+    story_file = './data/original/source_texts.csv'
+    story_df = pd.read_csv(story_file)
+    # Train-Val split
+    train_file = './data/train_val_split_csv/train.csv'
+    train_df = pd.read_csv(train_file)
+    val_file = './data/train_val_split_csv/val.csv'
+    val_df = pd.read_csv(val_file)
 
-# %%
-train_inps = construct_t5_input(train_story, train_answer)
-val_inps = construct_t5_input(val_story, val_answer)
+    train_story, train_answer, train_question = get_parallel_corpus(train_df, story_df)
+    val_story, val_answer, val_question = get_parallel_corpus(val_df, story_df)
 
-# %%
-train_input_ids, train_attention_mask, train_labels = get_t5_encoding(train_inps, train_question)
-val_input_ids, val_attention_mask, val_labels = get_t5_encoding(val_inps, val_question)
-print('Tokenized Data!')
-
-# %%
-train_dataset = FairyDataset(train_input_ids, train_attention_mask, train_labels)
-val_dataset = FairyDataset(val_input_ids, val_attention_mask, val_labels)
-print('Created Pytorch Dataset')
-
-# %%
-batch_size = 8
-train_dataloader = get_dataloader(batch_size, train_dataset)
-valid_dataloader = get_dataloader(batch_size, val_dataset, datatype='val')
-print('Loaded Dataloader!')
-
-# %%
-wandb.login()
-
-# %%
-max_epochs = 5
-model = FinetuneT5(num_train_epochs=max_epochs, lr=3e-4)
-
-# %%
-# Trainig code
-
-wandb_logger = WandbLogger(name='FinetuneT5', project='Quest_Gen_Challenge')
-
-early_stop_callback = EarlyStopping(
-    monitor='validation_loss',
-    patience=3,
-    strict=False,
-    verbose=False,
-    mode='min'
-)
-
-lr_monitor = LearningRateMonitor(logging_interval='step')
+    train_inps = construct_t5_input(train_story, train_answer)
+    val_inps = construct_t5_input(val_story, val_answer)
 
 
-trainer = Trainer(accelerator='gpu', devices=1, 
-                  default_root_dir="./Checkpoints", 
-                  logger=wandb_logger, 
-                  max_epochs=max_epochs)
-#                  callbacks=[early_stop_callback, lr_monitor])
+    train_input_ids, train_attention_mask, train_labels = get_t5_encoding(train_inps, train_question)
+    val_input_ids, val_attention_mask, val_labels = get_t5_encoding(val_inps, val_question)
+    print('Tokenized Data!')
 
 
-trainer.fit(model)
+    train_dataset = FairyDataset(train_input_ids, train_attention_mask, train_labels)
+    val_dataset = FairyDataset(val_input_ids, val_attention_mask, val_labels)
+    print('Created Pytorch Dataset')
 
-# %%
-save_directory = './Checkpoints'
-model.model.save_pretrained(save_directory)
+
+    batch_size = args.batch_size
+    train_dataloader = get_dataloader(batch_size, train_dataset)
+    valid_dataloader = get_dataloader(batch_size, val_dataset, datatype='val')
+    print('Loaded Dataloader!')
+
+    wandb.login()
+
+    max_epochs = args.num_epochs
+    model = FinetuneT5(model_name = args.model_name, num_train_epochs=max_epochs, lr=args.learning_rate)
+
+    # Trainig code
+    wandb_logger = WandbLogger(name=args.run_name, project='Quest_Gen_Challenge')
+
+    early_stop_callback = EarlyStopping(
+        monitor='validation_loss',
+        patience=3,
+        strict=False,
+        verbose=False,
+        mode='min'
+    )
+
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    
+    save_directory = os.path.join('./code/finetune/Checkpoints', args.run_name)
+    save_checkpoint =  ModelCheckpoint(dirpath=save_directory, monitor='validation_loss', save_top_k=1)
+
+    trainer = Trainer(accelerator='gpu', devices=1, 
+                    default_root_dir=save_directory, 
+                    logger=wandb_logger, 
+                    max_epochs=max_epochs,
+                    callbacks=[early_stop_callback, lr_monitor, save_checkpoint])
+
+    trainer.fit(model)
+
+    # if not os.path.exists(save_directory):
+    #     os.mkdir(save_directory)
+    # model.model.save_pretrained(save_directory)
