dbe527a7b9374a5fdcf2c5265f839673584a0966
Nish-19 <nischalrokz19@gmail.com>
2022-12-15 22:00:19 -0500

    update bash values

--- codex_augment_train.sh
+++ codex_augment_train.sh
@@ -5,9 +5,13 @@ const=0.5
 
 for i in $(seq .1 .2 1); 
 do 
+    echo 'Training with Lambda:' $i
+    python -m code.finetune.finetune_data_aug -W -E 10 -D 3 -MT T -MN google/flan-t5-large -N flan_t5_large -LAM $i 
+    : '
     if (( $(echo "$i != $const" |bc -l) ));
     then
         echo 'Training with Lambda:' $i
         python -m code.finetune.finetune_data_aug -W -E 10 -D 3 -MT T -MN google/flan-t5-large -N flan_t5_large -LAM $i 
     fi
+    '
 done
